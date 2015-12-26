b1c485c3e2de785e553f554c26177fce364d3bae
Paris <paris@sourcelair.com>
2015-12-26 16:40:38 +0200

    🐙

---
+++ README.md
@@ -0,0 +1,3 @@
+# Theater
+
+First-class Markdown template previewing.
--- theater/routes/index.js
+++ theater/routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var files = fs.readdirSync(process.env.THEATER_DIR).filter(function (file) {
+    var path = process.env.THEATER_DIR + '/' + file;
+    if (file.match(/\.(md|markdown)$/i) && !fs.statSync(path).isDirectory()) {
+      return true;
+    }
+    return false;
+  });
+
   res.render('index', {
-    title: 'Theater'
+    title: 'Theater',
+    files: files
   });
 });
 
