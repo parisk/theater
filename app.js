c4ba328f78e29ff817941be0a02e5e43e94e24f5
Paris <paris@sourcelair.com>
2016-01-05 17:29:52 +0200

    Load libraries from node modules

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/preview', preview);
 app.use('/static', express.static(__dirname + '/static'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
