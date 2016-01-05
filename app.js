32a2c761768b847c0058cc84cd9838261aba4ce6
Paris <paris@sourcelair.com>
2016-01-05 17:22:42 +0200

    REALLY fix static files

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/preview', preview);
-app.use('/static', express.static('static'));
+app.use('/static', express.static(__dirname + '/static'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
