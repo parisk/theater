48c718206fb16a1c9300e302d35a22655bbb41bc
Paris <paris@sourcelair.com>
2016-01-05 17:18:31 +0200

    Fix static serving

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/preview', preview);
+app.use('/static', express.static('static'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
